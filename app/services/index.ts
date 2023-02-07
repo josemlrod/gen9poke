c05df1fee334aa88e07c1865c94793245e99bcc5
josemlrod <mrjosemlrod@gmail.com>
2023-02-06 22:34:44 -0500

    export pokemon types

--- app/services/index.ts
+++ app/services/index.ts
@@ -5,9 +5,14 @@ import {
   getPaldeaPokemonData,
 } from "./pokemon";
 
+import type { Pokemon, PokemonEntry, PokemonName } from "./pokemonTypes";
+
 export {
   fetchPaldeaPokemonIds,
   fetchPokemonData,
   fetchPokemonSpecies,
   getPaldeaPokemonData,
+  Pokemon,
+  PokemonEntry,
+  PokemonName,
 };
