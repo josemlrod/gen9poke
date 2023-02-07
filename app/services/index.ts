491fe526426083c9e845590d6bad09487e2f0c69
josemlrod <mrjosemlrod@gmail.com>
2023-02-06 21:51:57 -0500

    add all available services to index.ts

---
+++ app/services/index.ts
@@ -0,0 +1,13 @@
+import {
+  fetchPaldeaPokemonIds,
+  fetchPokemonData,
+  fetchPokemonSpecies,
+  getPaldeaPokemonData,
+} from "./pokemon";
+
+export {
+  fetchPaldeaPokemonIds,
+  fetchPokemonData,
+  fetchPokemonSpecies,
+  getPaldeaPokemonData,
+};
