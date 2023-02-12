72c15be1a32b8ebd7f8645c63c31bd540208dcce
josemlrod <mrjosemlrod@gmail.com>
2023-02-12 01:26:41 -0500

    link to PokemonDetails screen from pokemonCard

--- app/components/pokemonCard.tsx
+++ app/components/pokemonCard.tsx
@@ -9,29 +9,29 @@ type Props = {
   pokemon: Pokemon;
 };
 
-export function PokemonCard({ pokemon }: Props) {
+export function PokemonCard({ pokemon: { id, name, types } }: Props) {
   const [
     {
       type: { name: pokemonMainTypeName },
     },
-  ] = pokemon.types;
+  ] = types;
   const cardBackground = getPokemonCardBgColor(pokemonMainTypeName);
   return (
     <a
       className={`flex flex-col relative rounded-xl p-4 shadow-2xl`}
-      href="/"
+      href={`/${name.toLowerCase()}`}
       style={{ backgroundColor: cardBackground, minWidth: 100 }}
     >
-      <p className="font-bold text-gray-200">{pokemon.name}</p>
+      <p className="font-bold text-gray-200">{name}</p>
       <div className="flex flex-row justify-between items-center">
         <div className="grid gap-1 grid-cols-2">
-          <PokemonTypes types={pokemon.types} />
+          <PokemonTypes types={types} />
         </div>
         <img
           alt="pokemon"
           className="w-20 h-20"
           src={`https://assets.pokemon.com/assets/cms2/img/pokedex/full/${formatPokemonId(
-            pokemon.id
+            id
           )}.png`}
         />
       </div>
